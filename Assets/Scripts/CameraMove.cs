8e7b79c3b06b4bb1e37cf6066dfeadaf3a5ad607
essi <essi@v228c.visitor.asm.fi>
2017-08-04 19:31:19 +0300

    camera move

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+	Vector3 targetPos, origPos;
+	// Use this for initialization
+	void Start () {
+		targetPos = transform.position;
+		origPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.mousePosition.x < 300)
+			targetPos = new Vector3(2.2f, transform.position.y, transform.position.z);
+		else if (Input.mousePosition.x > 1620)
+			targetPos = new Vector3(-2.2f, transform.position.y, transform.position.z);
+		else
+			targetPos = origPos;
+
+		transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime*0.5f);
+	}
+}
