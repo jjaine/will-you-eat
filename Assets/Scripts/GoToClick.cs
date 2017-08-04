7437678d96d0240901c4af3e8a891f25494ba5b3
essi <essi@v228c.visitor.asm.fi>
2017-08-04 15:20:23 +0300

    first version of the move script

---
+++ Assets/Scripts/GoToClick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToClick : MonoBehaviour {
+
+	float x,y;
+	Vector3 targetPos;
+	GameObject character;
+
+	// Use this for initialization
+	void Start () {
+		character = GameObject.FindWithTag("Player");
+		targetPos = character.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		x = Input.mousePosition.x;
+    	y = Input.mousePosition.y;
+    	character.transform.position = targetPos;
+	}
+
+	void OnMouseDown(){
+		targetPos = Camera.main.ScreenToWorldPoint(new Vector3(x,y,9.0f));
+		Debug.Log("Click at " +targetPos.x+" "+targetPos.y);
+	}			
+}
